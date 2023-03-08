a703833b5c6d0e43f10299c71262a8b9128f85a0
sachdevavaibhav <sachdevavaibhav.2001@gmail.com>
2023-03-08 11:49:14 +0530

    fixed event timings

--- src/components/Events/EventDetail.js
+++ src/components/Events/EventDetail.js
@@ -1,6 +1,6 @@
 export const eventDetail = [
     {
-        title: 'Hackathon',
+        title: 'Online Hackathon',
         text: 'A 24 Hour Hackathon',
         time: '10:00 am',
         link: {
@@ -29,16 +29,7 @@ export const eventDetail = [
     {
         title: 'FIFA',
         text: 'PS4 Gaming Tournament',
-        time: '10:00 am',
-        link: {
-            url: 'https://github.com/sachdevavaibhav',
-            text: 'Register Now'
-        }
-    },
-    {
-        title: 'Treasure Hunt',
-        text: 'Find treasure with help of clue',
-        time: '10:00 am',
+        time: '11:30 am',
         link: {
             url: 'https://github.com/sachdevavaibhav',
             text: 'Register Now'
@@ -47,7 +38,16 @@ export const eventDetail = [
     {
         title: 'Meme Making Competiton',
         text: 'Make memes and win exciting prizes',
-        time: '10:00 am',
+        time: '11:30 am',
+        link: {
+            url: 'https://github.com/sachdevavaibhav',
+            text: 'Register Now'
+        }
+    },
+    {
+        title: 'Treasure Hunt',
+        text: 'Find treasure with help of clue',
+        time: '12:00 pm',
         link: {
             url: 'https://github.com/sachdevavaibhav',
             text: 'Register Now'
@@ -56,10 +56,10 @@ export const eventDetail = [
     {
         title: 'Open Mic',
         text: 'Showcase your talent with RJ Naved',
-        time: '10:00 am',
+        time: '3:00 pm',
         link: {
             url: 'https://github.com/sachdevavaibhav',
             text: 'Register Now'
         }
-    }
+    },
 ]
