81c746d0ef3d671c6fe875d0230cdbc491eef89d
sachdevavaibhav <sachdevavaibhav.2001@gmail.com>
2023-03-13 22:54:36 +0530

    general sponsor

--- src/components/Sponsors/SponsorData.js
+++ src/components/Sponsors/SponsorData.js
@@ -3,6 +3,7 @@ import polygonLogo from '../../assets/images/Polygon_Logo-Colored.png'
 import filecoinLogo from '../../assets/images/Filecoin_Logo-Colored.png'
 import solanaLogo from '../../assets/images/Solana_Logo-Colored.png'
 import replitLogo from '../../assets/images/Replit_Logo-Colored.png'
+import interviewCake from '../../assets/images/interview_cake.png'
 
 export const sponsorData = [
     {
@@ -16,6 +17,10 @@ export const sponsorData = [
     {
         title: 'Co-Powered By',
         details: [{imgSrc: solanaLogo, link: 'https://solana.com '}, {imgSrc: filecoinLogo, link: 'https://filecoin.io '}, {imgSrc: replitLogo, link: 'https://replit.com '}]
+    },
+    {
+        title: 'General Sponsors',
+        details: [{imgSrc: interviewCake, link: 'https://www.interviewcake.com'},]
     },
     
 ] 
