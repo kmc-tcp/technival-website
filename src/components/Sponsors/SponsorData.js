0e11f7fe3e752b70d29012df1b18c29aed5462e2
sachdevavaibhav <sachdevavaibhav.2001@gmail.com>
2023-03-14 18:54:01 +0530

    updated sponsors

--- src/components/Sponsors/SponsorData.js
+++ src/components/Sponsors/SponsorData.js
@@ -3,7 +3,10 @@ import polygonLogo from '../../assets/images/Polygon_Logo-Colored.png'
 import filecoinLogo from '../../assets/images/Filecoin_Logo-Colored.png'
 import solanaLogo from '../../assets/images/Solana_Logo-Colored.png'
 import replitLogo from '../../assets/images/Replit_Logo-Colored.png'
+import printsabkuchLogo from '../../assets/images/printsabkuch.png'
 import interviewCake from '../../assets/images/interview_cake.png'
+import interviewBuddy from '../../assets/images/InterviewBuddy.png'
+import axure from '../../assets/images/Axure.png'
 
 export const sponsorData = [
     {
@@ -19,8 +22,13 @@ export const sponsorData = [
         details: [{imgSrc: solanaLogo, link: 'https://solana.com '}, {imgSrc: filecoinLogo, link: 'https://filecoin.io '}, {imgSrc: replitLogo, link: 'https://replit.com '}]
     },
     {
+        title: 'Printing Partner',
+        details: [{imgSrc: printsabkuchLogo, link: 'https://www.printsabkuch.com'},]
+    },
+    {
         title: 'General Sponsors',
-        details: [{imgSrc: interviewCake, link: 'https://www.interviewcake.com'},]
+        details: [{imgSrc: interviewCake, link: 'https://www.interviewcake.com'},
+        {imgSrc: interviewBuddy, link: 'https://www.interviewbuddy.in'}, {imgSrc: axure, link: 'https://www.axure.com'}]
     },
     
 ] 
