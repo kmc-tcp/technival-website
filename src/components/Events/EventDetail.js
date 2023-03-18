71c46cb430077cf1a905b327801fa684303654cd
sachdevavaibhav <sachdevavaibhav.2001@gmail.com>
2023-03-18 14:39:14 +0530

    title updated

--- src/components/Events/EventDetail.js
+++ src/components/Events/EventDetail.js
@@ -14,52 +14,52 @@ export const eventDetail = [
         text: 'Competitive Programming',
         time: '10:30 am',
         link: {
-            url: 'https://github.com/sachdevavaibhav',
+            url: 'https://forms.gle/yaYcLfTFkTzfZZ9C7',
             text: 'Register Now'
         }
     },
     {
-        title: 'Graphical Sketching',
-        text: 'Sketch your ideas',
+        title: 'Artista',
+        text: 'Digital Art Competition',
         time: '10:30 am',
         link: {
-            url: 'https://github.com/sachdevavaibhav',
+            url: 'https://forms.gle/yaYcLfTFkTzfZZ9C7',
             text: 'Register Now'
         }
     },
     {
         title: 'FIFA',
-        text: 'PS4 Gaming Tournament',
+        text: 'PS5 Gaming Tournament',
         time: '11:30 am',
         link: {
-            url: 'https://github.com/sachdevavaibhav',
+            url: 'https://forms.gle/hoavVGh1wutZzynp8',
             text: 'Register Now'
         }
     },
     {
-        title: 'Meme Making Competiton',
-        text: 'Make memes and win exciting prizes',
+        title: 'Meme War',
+        text: 'Make memes and win exciting prizes.',
         time: '11:30 am',
         link: {
-            url: 'https://github.com/sachdevavaibhav',
+            url: 'https://forms.gle/yaYcLfTFkTzfZZ9C7',
             text: 'Register Now'
         }
     },
     {
-        title: 'Treasure Hunt',
-        text: 'Find treasure with help of clue',
+        title: 'Holmes: A Game of Treasure',
+        text: 'Find treasure with help of clues.',
         time: '12:00 pm',
         link: {
-            url: 'https://github.com/sachdevavaibhav',
+            url: 'https://forms.gle/UNKNWRNb66ehTwUn8',
             text: 'Register Now'
         }
     },
     {
-        title: 'Open Mic',
+        title: 'Rangmanch',
         text: 'Showcase your talent with RJ Naved',
         time: '3:00 pm',
         link: {
-            url: 'https://github.com/sachdevavaibhav',
+            url: 'https://forms.gle/vxmAyN9gLAvZ2mS18',
             text: 'Register Now'
         }
     },
